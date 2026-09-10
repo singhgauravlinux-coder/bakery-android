2052f809a7682f2ac9bf583ba0cef3c8f31f0b92
singhgauravlinux-coder <singhgauravlinux@gmail.com>
2026-09-10 15:25:01 +0530

    Add compiler argument for ExperimentalMaterial3Api

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -103,6 +103,7 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += listOf("-opt-in=androidx.compose.material3.ExperimentalMaterial3Api")
     }
 
     packaging {
